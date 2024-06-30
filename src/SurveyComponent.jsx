530efd25dc390324a414048b8dbdeb71009c348e
Orisnow <123253951+Orisnow@users.noreply.github.com>
2024-06-30 22:48:27 +0800

    Update SurveyComponent.jsx

--- src/SurveyComponent.jsx
+++ src/SurveyComponent.jsx
@@ -4,7 +4,6 @@ import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.min.css";
 import "./index.css";
 import { json } from "./json";
-import { db } from './firebase'; // 导入Firebase配置
 
 function SurveyComponent() {
     const survey = new Model(json);
@@ -14,23 +13,7 @@ function SurveyComponent() {
     return (<Survey model={survey} />);
 }
 
-const SurveyComponent = () => {
-  const onComplete = (survey) => {
-    const data = survey.data;
-    console.log("Survey results: ", result.data);
-    // 将数据保存到Firestore
-    db.collection('surveyResults')
-      .add(data)
-      .then(() => {
-        console.log('Document successfully written!');
-      })
-      .catch((error) => {
-        console.error('Error writing document: ', error);
-      });
-  };
 
-  return <Survey json={surveyJSON} onComplete={onComplete} />;
-};
 
 
 <<<<<<< HEAD
